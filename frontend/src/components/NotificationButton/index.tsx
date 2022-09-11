7d0bfa1cca04fd85958d6e70f7b1ad83a1d4b6cf
EliasRamiresJr <ramires_junior11@hotmail.com>
2022-09-10 20:00:29 -0400

    Send notification

--- frontend/src/components/NotificationButton/index.tsx
+++ frontend/src/components/NotificationButton/index.tsx
@@ -1,10 +1,23 @@
+import axios, { Axios } from "axios"
 import ico from "../../assets/img/notification-icon.svg"
+import { BASE_URL } from "../../utlis/request"
 import "./styles.css"
 
-function NotificationButton() {
+type Props = {
+    saleId: number
+}
+
+function handleClick(id :number){
+    axios (`${BASE_URL}/sales/${id}/notification`)
+    .then(response => {
+        console.log("SUCESSO")
+    })
+}
+
+function NotificationButton({ saleId }) {
     return (
-        <div className="dsmeta-red-btn">
-            <img src={ico}alt="Notificar" />
+        <div className="dsmeta-red-btn" onClick={() => handleClick(saleId)}>
+            <img src={ico} alt="Notificar" />
         </div>
     )
 }
